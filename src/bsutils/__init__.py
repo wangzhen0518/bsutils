3f8021229f5da6b185d8a2209bf31fdb82d1ba16
wangzhen0518 <wangzhen0518@126.com>
2025-04-03 17:05:39 +0800

    use __init__ to re-export functions

--- src/bsutils/__init__.py
+++ src/bsutils/__init__.py
@@ -1,2 +1,4 @@
-def main() -> None:
-    print("Hello from bsutils!")
+from .file import pure_file_name
+from .json import iter_json_file, get_item_num, load_json_file, json_to_jsonl, jsonl_to_json, write_json_file
+
+__all__ = ["pure_file_name", "iter_json_file", "get_item_num", "load_json_file", "json_to_jsonl", "jsonl_to_json", "write_json_file"]
