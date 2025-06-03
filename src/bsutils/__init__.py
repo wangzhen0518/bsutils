71ec699a6c3c126eaf684136ed985062f8911942
wangzhen0518 <wangzhen0518@126.com>
2025-06-03 19:25:14 +0800

    release v0.0.4-post1

--- src/bsutils/__init__.py
+++ src/bsutils/__init__.py
@@ -2,18 +2,20 @@ from .file import pure_file_name
 from .io import OutputCapturer
 from .iterator import Iterator
 from .json import get_item_num, iter_json_file, json_to_jsonl, jsonl_to_json, load_json_file, write_json_file
-from .option import Option, Some, Null
-from .result import Err, Ok, Result
+from .option import Option, Some, Null, optionalify
+from .result import Err, Ok, Result, resultify
 from .version import __version__
 
 __all__ = [
     "__version__",
     "Option",
+    "optionalify",
     "Some",
     "Null",
     "Result",
     "Ok",
     "Err",
+    "resultify",
     "Iterator",
     "OutputCapturer",
     "pure_file_name",
